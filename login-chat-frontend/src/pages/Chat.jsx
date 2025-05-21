57a60762ac2238adaa806343921fc1843939780c
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-05-21 16:01:02 -0300

    Update project

--- login-chat-frontend/src/pages/Chat.jsx
+++ login-chat-frontend/src/pages/Chat.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
-import UserList from '../components/UserList'
+import UserList from '../components/UserList';
 import ChatBox from '../components/ChatBox';
+import './ChatHeader.css'; // Vamos criar este arquivo para os estilos
 
 const Chat = () => {
   const { user, logout } = useAuth();
@@ -10,8 +11,16 @@ const Chat = () => {
   return (
     <div className="chat-container">
       <header className="chat-header">
-        <p>Logado como: <strong>{user.username}</strong></p>
-        <button onClick={logout}>Sair</button>
+        <h2>Bem vindo, <strong>{user.username}</strong></h2>
+        <div className="logout-btn-container">
+          <button className="logout-btn" onClick={logout}>
+            <svg width="180px" height="60px" viewBox="0 0 180 60" className="border">
+              <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
+              <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
+            </svg>
+            <span className='logout'>SAIR</span>
+          </button>
+        </div>
       </header>
 
       <div className="chat-body">
